32adb0ce3e0dc5d744cc958db5f59ed6f90985e4
Anshika Gangwar <anshikagangwar341@gmail.com>
2025-04-29 18:46:22 +0530

    Check input and Type

--- FahToCelcius.py
+++ FahToCelcius.py
@@ -1,5 +1,11 @@
-f = 100
-c = (f-32) * 5/9
-print(c)
-ci = (f-32) * 5//9
-print(ci)
+# f = 100
+# c = (f-32) * 5/9
+# print(c)
+# ci = (f-32) * 5//9
+# print(ci)
+
+a = input()
+b = input()
+print(type(a))
+s = a+b
+print(s)
